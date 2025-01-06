0540ff2fcadca31bce02df41817f8f6ef8209257
Sunnatulloh06 <sunnatullohsaidzoda@gmail.com>
2025-01-06 17:38:41 +0500

    Created a new branch 4

--- app/src/main/java/com/example/login_io/MainActivity.kt
+++ app/src/main/java/com/example/login_io/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.login_io.navigation.NavScreen
 
 class MainActivity : ComponentActivity() {
@@ -15,3 +22,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+
--- app/src/main/java/com/example/login_io/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/login_io/presentation/login/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.example.login_io.presentation.login
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -9,9 +11,9 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -28,6 +30,7 @@ import com.example.login_io.components.CommonButton
 import com.example.login_io.components.CommonCard
 import com.example.login_io.components.CommonTextButton
 import com.example.login_io.components.CommonTextField
+import com.example.login_io.components.CommonTitle
 import com.example.login_io.navigation.Screen
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -49,102 +52,126 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         modifier = Modifier.fillMaxSize()
     ) {
-        // Градиентный фон
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color(0xFF2567E8),
-                            Color(0xFF1CE6DA)
-                        )
-                    )
-                )
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
+                modifier = Modifier.fillMaxSize()
             ) {
-                // Логотип
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 100.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.top_icon),
-                            contentDescription = "Main Icon",
-                            contentScale = ContentScale.Fit,
-                            modifier = Modifier.size(25.dp),
-                            alignment = Alignment.Center,
-                            colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(Color.White)
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(
-                            "Logoipsum",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.White
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(40.dp))
-
-                // Белый блок с формой
+                        .weight(1f)
+                        .background(Color(0xFF2567E8))
+                )
                 Box(
                     modifier = Modifier
-                        .width(400.dp)
-                        .height(680.dp)
-                        .background(Color.White, shape = RoundedCornerShape(14.dp))
-                        .padding(start = 16.dp, end = 16.dp, top = 40.dp)
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .background(Color(0xFFF5F5F5))
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp, vertical = 18.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(100.dp))
+
+                // Логотип
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp, bottom = 20.dp)
                         .align(Alignment.CenterHorizontally),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.top_icon),
+                        contentDescription = "Main Icon",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.size(25.dp),
+                        alignment = Alignment.Center,
+                        colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(Color.White)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    CommonTitle(
+                        text = "Sign in to your"
+                    )
+                    CommonTitle(
+                        text = "Acccount"
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    Text(
+                        "Enter your email and password to log in",
+                        color = Color.White
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                // Белый бокс
+                Box(
+                    modifier = Modifier
+                        .width(382.dp)
+                        .height(490.dp)
+                        .background(Color.White, shape = RoundedCornerShape(14.dp))
+                        .padding(16.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        // Заголовок
-                        Text(
-                            "Login",
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontSize = 45.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        CommonTextButton(
-                            text1 = "Don't have an account?",
-                            text2 = "SignUp",
-                            onClick = {
-                                navController.navigate(Screen.Signup_Screen.route)
-                            },
-                            horizontalAlignment = Arrangement.Center
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        CommonCard(
+                            text = "Continue with Google",
+                            iconResId = R.drawable.google
                         )
                         Spacer(modifier = Modifier.height(24.dp))
 
-                        // Поле Email
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 8.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Divider(
+                                modifier = Modifier.weight(1f),
+                                color = Color.Gray,
+                                thickness = 1.dp
+                            )
+                            Text(
+                                text = "Or login with",
+                                color = Color.Gray,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                            Divider(
+                                modifier = Modifier.weight(1f),
+                                color = Color.Gray,
+                                thickness = 1.dp
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // Поля ввода
                         CommonTextField(
-                            label = "Email",
                             value = view.email,
                             onValueChange = { view.email = it },
                             modifier = Modifier.fillMaxWidth(),
                             keyboardType = KeyboardType.Email
                         )
-                        Spacer(modifier = Modifier.height(16.dp))
+                        Spacer(modifier = Modifier.height(18.dp))
 
-                        // Поле Password
                         var passwordVisible by remember { mutableStateOf(false) }
                         CommonTextField(
-                            label = "Password",
                             value = view.password,
                             onValueChange = { view.password = it },
                             modifier = Modifier.fillMaxWidth(),
@@ -164,7 +191,6 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
 
                         Spacer(modifier = Modifier.height(8.dp))
 
-                        // Remember Me и Forgot Password
                         var rememberMe by remember { mutableStateOf(false) }
 
                         Row(
@@ -176,7 +202,8 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
                         ) {
                             Row(
                                 modifier = Modifier.padding(start = 10.dp),
-                                verticalAlignment = Alignment.CenterVertically
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
                             ) {
                                 Checkbox(
                                     checked = rememberMe,
@@ -187,7 +214,8 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
                                 Text(
                                     text = "Remember me",
                                     color = Color.Gray,
-                                    fontSize = 16.sp
+                                    fontSize = 16.sp,
+                                    modifier = Modifier.clickable { rememberMe = !rememberMe }
                                 )
                             }
 
@@ -201,9 +229,8 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
 
                         Spacer(modifier = Modifier.height(24.dp))
 
-                        // Кнопка Login
                         CommonButton(
-                            label = "Login",
+                            label = "Log In",
                             onClick = {
                                 view.login {
                                     navController.navigate(Screen.Home_Screen.route)
@@ -211,37 +238,17 @@ fun LoginScreen(navController: NavController, view: CommonViewModel) {
                             }
                         )
 
-                        Spacer(modifier = Modifier.height(24.dp))
-
-                        // Разделитель OR
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Divider(
-                                modifier = Modifier.weight(1f),
-                                color = Color.Gray,
-                                thickness = 1.dp
-                            )
-                            Text(
-                                text = "OR",
-                                color = Color.Gray,
-                                fontWeight = FontWeight.Bold,
-                                modifier = Modifier.padding(horizontal = 8.dp)
-                            )
-                            Divider(
-                                modifier = Modifier.weight(1f),
-                                color = Color.Gray,
-                                thickness = 1.dp
-                            )
-                        }
-
-                        Spacer(modifier = Modifier.height(24.dp))
-
-                        // Кнопки социальных сетей
-                        CommonCard(text = "Continue with Google", iconResId = R.drawable.google)
-                        Spacer(modifier = Modifier.height(10.dp))
-                        CommonCard(text = "Continue with Facebook", iconResId = R.drawable.facebook)
+                        Spacer(modifier = Modifier.height(28.dp))
+                        CommonTextButton(
+                            text1 = "Don't have an account?",
+                            text2 = "SignUp",
+                            onClick = {
+                                navController.navigate(Screen.Signup_Screen.route)
+                            },
+                            horizontalAlignment = Arrangement.Center,
+                            color1 = Color.Black,
+                            color2 = MaterialTheme.colorScheme.primary
+                        )
                     }
                 }
             }
